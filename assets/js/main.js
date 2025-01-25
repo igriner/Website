c1d72808dba9f930c361fc4b72aa75dfd3be4ca0
igriner <igrin3750@gmail.com>
2025-01-25 13:09:35 -0500

    Updated information and picture

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,17 @@ window.addEventListener('DOMContentLoaded', () => {
         logoSpan.forEach((span, idx) => {
             setTimeout(() => {
                 span.classList.add('active')
-            }, (idx + 1) * 400)
+            }, (idx + 1) * 600)
         })
         setTimeout(() => {
             logoSpan.forEach((span, idx) => {
                 span.classList.remove('active')
                 span.classList.add('fade')
-            }, (idx + 1) * 50)
+            }, (idx + 1) * 100)
         }, 2000)
         setTimeout(() => {
             intro.style.top = '-100vh';
-        }, 2300)
+        }, 2500)
     })
 })
 AOS.init();
